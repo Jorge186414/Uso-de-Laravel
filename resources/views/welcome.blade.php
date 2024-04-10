ecd6c0f9f6b38e507aac2ca01b934d1cc955ee5f
Jorge Miranda <jorgaismiranda@gmail.com>
2024-04-10 08:44:37 -0600

    Fix: i dont save changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel | TESJI ISIC 3601</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body >
-        <h1>Bienvenido a Laravel</h1>
-        <h3>Hola Mundo !! </h3>
-        <nav>
-            <ul>
-                <li><a href="#">Principal</a></li>
-                <li><a href="#">Nosotros</a></li>
-                <li><a href="#">Venta Laptops</a></li>
-                <li><a href="#">Contactanos</a></li>
-            </ul>
-        </nav>
-    </body>
+    <title>Laravel | TESJI ISIC 3601</title>
+
+</head>
+
+<body>
+    <h1>Bienvenido a Laravel</h1>
+    <h3>Hola Mundo !! </h3>
+    <nav>
+        <ul>
+            <li><a href="#">Principal</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Venta Laptops</a></li>
+            <li><a href="#">Contactanos</a></li>
+        </ul>
+    </nav>
+</body>
+
 </html>
